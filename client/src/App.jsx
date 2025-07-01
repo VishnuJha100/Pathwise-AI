8d9137e270317281c76bc02938023d30523ef492
Vishnu Deb Jha <jhavishnu100@gmail.com>
2025-07-01 14:13:29 +0530

    Initial project setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">🚀 Pathwise-AI Coming Soon</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotnev from "dotenv";
+
+dotnev.config();
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 8000;
+
+// DB connection
+mongoose.connect(process.env.MONGO_URI, {})
+    .then(() => {
+        console.log("MongoDB Connected!");
+        app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+    })
+    .catch((err) => console.error(err));
