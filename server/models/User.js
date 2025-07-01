a391d32125fc18b9a6112ea3a5e4f5f24ec84d02
Vishnu Deb Jha <jhavishnu100@gmail.com>
2025-07-01 14:20:21 +0530

    User model done

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    skills: {
+        type: [String],
+        default: []
+    }
+}, { timeseries: true });
+
+export default mongoose.model(User, userSchema);
